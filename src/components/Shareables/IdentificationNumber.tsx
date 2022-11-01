d7a24ea10bfecdeaed3a1d05610b8c2b848aae04
Adeshina's <badmusadeshinna@gmail.com>
2022-11-01 10:58:09 +0100

    identification type and number component

---
+++ src/components/Shareables/IdentificationNumber.tsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+
+// type Props = {}
+
+// const IdentificationNumber = (props: Props) => {
+//   return (
+//     <div className='flex items-center'>
+//       Identification Number{' '}
+//       <svg className='w-4 h-4' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+//         <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'></path>
+//       </svg>
+//       <div className='flex  border-b border-y-[#8F8F8F] ml-10 w-[250px]'>
+//         <input
+//           type='text'
+//           value={text}
+//           placeholder={`Enter your ${verificationMode}`}
+//           onChange={handleVerification}
+//           maxLength={20}
+//           className='w-full'
+//         />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default IdentificationNumber
