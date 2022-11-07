a74af2d31cd6bdcc25d31c679a4dfcd3b80e8960
owoh princewill <princewillowoh@gmail.com>
2022-11-07 09:14:26 +0100

    dashboard UI design

--- src/components/Shareables/index.ts
+++ src/components/Shareables/index.ts
@@ -1,3 +1,5 @@
+
 export { default as RemoveButton } from './RemoveButton'
 export { default as AppAlert } from './AppAlert'
 export { default as AppModal } from './AlertModal'
+export { default as QuickLinks } from './QuickLinks'
