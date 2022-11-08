26fcba2c7e665c452aac6e0d2140fff2aab70bd4
Bonaventure Chukwudi <bonarhyme@gmail.com>
2022-11-08 16:02:27 +0100

    Added layout for form

---
+++ src/components/Form/Form-UIs/Steps.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const Steps = (props: Props) => {
+  return <div className='flex justify-center min-h-[120px] border border-red-500'>Steps</div>
+}
+
+export default Steps
