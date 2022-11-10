561c571a6f99caab85a241f5907a7be68109f2a1
Bonaventure Chukwudi <bonarhyme@gmail.com>
2022-11-10 08:30:53 +0100

    Added redux to fetch published form

---
+++ src/redux/actions/FormManagement.actions.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { Dispatch } from 'redux'
+import { GET_FORM_FAIL, GET_FORM_REQUEST, GET_FORM_SUCCESS } from 'Redux/constants/FormManagement.constants'
+import { ReducersType } from 'Redux/store'
+
+const SERVER_URL = 'https://retailcore-customerservice.herokuapp.com/v1/form/'
+
+export const getFormAction = (formType: string) => async (dispatch: Dispatch, getState: (store: ReducersType) => ReducersType) => {
+  try {
+    dispatch({ type: GET_FORM_REQUEST })
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    const { data } = await axios.get(`${SERVER_URL}/published/type/${formType}`, config)
+
+    dispatch({ type: GET_FORM_SUCCESS, payload: data })
+    // localStorage.removeItem('form')
+  } catch (error) {
+    // localStorage.removeItem('form')
+    dispatch({
+      type: GET_FORM_FAIL,
+      payload: error?.response && error?.response?.data?.message,
+    })
+  }
+}
---
+++ src/redux/constants/FormManagement.constants.ts
@@ -0,0 +1,3 @@
+export const GET_FORM_REQUEST = 'GET_FORM_REQUEST'
+export const GET_FORM_SUCCESS = 'GET_FORM_SUCCESS'
+export const GET_FORM_FAIL = 'GET_FORM_FAIL'
