c35990b45649595310133656ca22301a79bb70a5
Adeshina's <badmusadeshinna@gmail.com>
2022-11-15 15:41:51 +0100

    ocr extration first test

--- src/components/Shareables/IdentificationTypeAndNumber.tsx
+++ src/components/Shareables/IdentificationTypeAndNumber.tsx
@@ -25,12 +25,15 @@ const IdentificationTypeAndNumber = ({ customerType, setIdentificationDetails }:
   const [identificationNumber, setIdentificationNumber] = useState<IdentificationNumberType>('')
   const [isVerified, setIsVerified] = useState<boolean | null>(null)
   const [status, setStatus] = useState<FieldStatus>('default')
+  const [isAlreadyACustomer, setIsAlreadyACustomer] = useState(false)
+
+  const MAX_FIELD_LENGTH = 10
 
   const handleVerification = async (ev: ChangeEvent<HTMLInputElement>) => {
     const { value } = ev.target
     setIdentificationNumber(value)
     setStatus('default')
-    if (value.length === 20) {
+    if (value.length === MAX_FIELD_LENGTH) {
       try {
         const response = await API.get(`/verification/${selectedIdentificationType}/${value.trim()}`)
         if (response.data && response.status == 200) {
@@ -40,8 +43,20 @@ const IdentificationTypeAndNumber = ({ customerType, setIdentificationDetails }:
             identificationType: selectedIdentificationType,
             identificationNumber: value.trim(),
           })
+          try {
+            const existingCustomer = await API.get(`/customer/id?field=${selectedIdentificationType}&id=${value.trim()}`)
+            // console.log('existingCustomer', existingCustomer)
+            if (existingCustomer.data && existingCustomer.status == 200) {
+              setIsAlreadyACustomer(true)
+            }
+          } catch (err) {
+            // console.log(err.response)
+            if (err.response && err.response.status === 404) {
+              setIsAlreadyACustomer(false)
+            }
+          }
         }
-        console.log('response', response)
+        // console.log('response', response)
       } catch (err) {
         console.error(err.message)
         setStatus('error')
@@ -85,12 +100,18 @@ const IdentificationTypeAndNumber = ({ customerType, setIdentificationDetails }:
             type='text'
             placeholder='Enter text'
             onChange={handleVerification}
-            maxLength={20}
+            maxLength={MAX_FIELD_LENGTH}
             readOnly={!selectedIdentificationType}
             value={identificationNumber}
           />
         </div>
+        {/* 9856409479 */}
       </div>
+      {isAlreadyACustomer && (
+        <div className='ml-auto'>
+          Customer profile already exists, <br /> <span className='underline cursor-pointer text-primay-main'>click here</span> to view
+        </div>
+      )}
     </div>
   )
 }
