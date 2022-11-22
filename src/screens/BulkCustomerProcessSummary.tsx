990faf377c20aecb291f73d3c6e668b726d57eb5
Adeshina's <badmusadeshinna@gmail.com>
2022-11-22 16:12:06 +0100

    activity log

--- src/screens/BulkCustomerProcessSummary.tsx
+++ src/screens/BulkCustomerProcessSummary.tsx
@@ -19,19 +19,19 @@ import { ReducersType } from 'Redux/store'
 import { ProcessDoneStateIcon, ProcessPendingStateIcon } from 'Assets/images'
 import { CancelIcon, ModifyIcon, SubmitIcon } from 'Assets/svgs'
 import { BulkProcessSummaryTable } from 'Components/BulkCreation/BulkProcessSummaryTable'
+import ActivityLog from 'Components/Shareables/ActivityLog'
 
 type Props = {
-  headerText: string,
-  customerType: "individual" | "sme"
+  headerText: string
+  customerType: 'individual' | 'sme'
 }
-
 
 export const BulkCustomerProcessSummary = memo(({ headerText, customerType }: Props) => {
   const naviate = useNavigate()
   const { bulkSummary } = useSelector<ReducersType>((state) => state.bulkProcessSummary) as BulkProcessSummaryTypes
 
   const onCancelCreation = useCallback(() => {
-    naviate("/")
+    naviate('/')
   }, [])
 
   return (
@@ -41,22 +41,21 @@ export const BulkCustomerProcessSummary = memo(({ headerText, customerType }: Pr
       </nav>
 
       <main className={`bg-background-dash relative flex mx-auto py-[20px] font-roboto px-[30px] gap-x-[20px] h-screen`}>
-        <section className={`w-[80%]`}>
+        <section className={`w-[75%]`}>
           <div className={`relative rounded-lg text-[#636363] font-[Inter] w-full h-full  min:h-full max:h-full overflow-auto bg-white pt-20`}>
             <div className={`relative ml-20 mb-20 h-[108px] w-[60%] rounded-[20px] border border-[#d2d2d2] flex justify-center items-center`}>
               <div className={`w-[80%] flex justify-center items-center`}>
                 <div className={`absolute bg-white border-none -top-3 left-7`}>Processing Status:</div>
                 <div className={`w-[90%] relative h-[50px]`}>
                   <div className={`w-full m-auto absolute inset-0 bg-[#d9d9d9] h-[10px]`}></div>
-                  <img className={`absolute m-auto inset-y-0 left-0 w-[36px] rounded-full`} src={ProcessDoneStateIcon} alt="" />
+                  <img className={`absolute m-auto inset-y-0 left-0 w-[36px] rounded-full`} src={ProcessDoneStateIcon} alt='' />
                   <div className={`absolute m-auto -bottom-[30px] -left-[50px] h-[30px] `}>Pending Submission</div>
-                  <img className={`absolute m-auto inset-y-0 right-0 w-[36px] rounded-full`} src={ProcessPendingStateIcon} alt="" />
+                  <img className={`absolute m-auto inset-y-0 right-0 w-[36px] rounded-full`} src={ProcessPendingStateIcon} alt='' />
                   <div className={`absolute m-auto -bottom-[30px] text-center -right-[60px] w-[150px] h-[30px]`}>Approval</div>
                 </div>
               </div>
             </div>
             <div className={`px-20 mt-20 h-[300px] overflow-auto`}>
-
               <BulkProcessSummaryTable
                 onSearchStringChange={undefined}
                 records={bulkSummary}
@@ -68,37 +67,30 @@ export const BulkCustomerProcessSummary = memo(({ headerText, customerType }: Pr
             <div className={`bg-white absolute m-auto bottom-2 right-2 w-[254px] h-[64px] flex justify-evenly items-center rounded-lg shadow-md`}>
               <button className={`flex flex-col justify-center items-center`}>
                 <ModifyIcon />
-                <div>
-                  Modify
-                </div>
+                <div>Modify</div>
               </button>
               <button className={`flex flex-col justify-center items-center`} onClick={onCancelCreation}>
                 <CancelIcon />
-                <div>
-                  Cancel
-                </div>
+                <div>Cancel</div>
               </button>
               <button className={`flex flex-col justify-center items-center`}>
                 <SubmitIcon />
-                <div>
-                  Submit Form
-                </div>
+                <div>Submit Form</div>
               </button>
             </div>
           </div>
         </section>
-        <section className={`w-[20%]`}>
-          <div className={`rounded-lg text-[#636363] text-[24px] leading-6 font-medium font-[Inter] tracking-wide w-full h-full bg-white pt-[25px] px-[20px]`}>
-            <div>
-              Activity Log
-              <hr className={`w-full mt-4 border border-[#CCCCCC]`} />
-            </div>
-            <div className={`mt-5 min-h-[70%] w-full max-h-[70%] overflow-auto`}>
 
+        <section className={`w-[25%]`}>
+          <div
+            className={`rounded-lg text-[#636363] text-[24px] leading-6 font-medium font-[Inter] tracking-wide w-full h-full bg-white pt-[25px] px-[20px]`}
+          >
+            <div>
+              <ActivityLog />
             </div>
+            <div className={`mt-5 min-h-[70%] w-full max-h-[70%] overflow-auto`}></div>
           </div>
         </section>
-
       </main>
     </>
   )
