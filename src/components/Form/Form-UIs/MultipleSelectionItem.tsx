e4605cf45b5485c79c6e40bd822a7b7cf4fdd522
Bonaventure Chukwudi <Bonarhyme@gmail.com>
2022-11-22 14:49:22 +0100

    finished handling form dropdown state

--- src/components/Form/Form-UIs/MultipleSelectionItem.tsx
+++ src/components/Form/Form-UIs/MultipleSelectionItem.tsx
@@ -3,9 +3,10 @@ import React, { useState } from 'react'
 type Props = {
   handleMultipleSelectedDropdownItem: (selected: string, action: 'remove' | 'add') => void
   selected: string
+  multipleSelectedDropdownItems: Array<string>
 }
 
-const MultipleSelectionItem = ({ handleMultipleSelectedDropdownItem, selected }: Props) => {
+const MultipleSelectionItem = ({ handleMultipleSelectedDropdownItem, selected, multipleSelectedDropdownItems }: Props) => {
   return (
     <div className='flex gap-2 py-2 px-3 items-center text-[#636363] hover:bg-slate-100'>
       <div className='relative inline-flex items-center cursor-pointer'>
@@ -13,6 +14,7 @@ const MultipleSelectionItem = ({ handleMultipleSelectedDropdownItem, selected }:
           type='checkbox'
           id='hhh'
           className={`cursor-pointer accent-primay-main w-4 h-4 text-primay-main bg-gray-100 rounded border-gray-300 focus:ring-primay-main`}
+          checked={typeof multipleSelectedDropdownItems.find((x) => x === selected) === 'string'}
           onChange={(e) => {
             let isChecked = e.target.checked
 
