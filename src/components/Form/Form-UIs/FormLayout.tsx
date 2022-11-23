25dae493d0ac78c4921015980121fbe186c97c12
Bonaventure Chukwudi <Bonarhyme@gmail.com>
2022-11-23 15:44:06 +0100

    Added backup state for phone input

--- src/components/Form/Form-UIs/FormLayout.tsx
+++ src/components/Form/Form-UIs/FormLayout.tsx
@@ -223,6 +223,8 @@ const FormLayout = ({
                   setFillingFormState={setFillingFormState}
                   publishedFormState={publishedFormState}
                   fillingFormState={fillingFormState}
+                  setBackupForSwitchFormState={setBackupForSwitchFormState}
+                  backupForSwitchFormState={backupForSwitchFormState}
                 />
               )
             }
