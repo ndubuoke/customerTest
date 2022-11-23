ca1790dbb2c575c222cf5a618883af6816f98290
Bonaventure Chukwudi <Bonarhyme@gmail.com>
2022-11-23 15:41:38 +0100

    Added backup state for form search and select

--- src/components/Form/Form-UIs/FormLayout.tsx
+++ src/components/Form/Form-UIs/FormLayout.tsx
@@ -208,6 +208,8 @@ const FormLayout = ({
                   setFillingFormState={setFillingFormState}
                   publishedFormState={publishedFormState}
                   fillingFormState={fillingFormState}
+                  setBackupForSwitchFormState={setBackupForSwitchFormState}
+                  backupForSwitchFormState={backupForSwitchFormState}
                 />
               )
             }
