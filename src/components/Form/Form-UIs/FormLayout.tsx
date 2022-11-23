900f4767520f2a02db95fcd794819e8883f0ec97
Bonaventure Chukwudi <Bonarhyme@gmail.com>
2022-11-23 16:02:10 +0100

    Added backup state for file upload

--- src/components/Form/Form-UIs/FormLayout.tsx
+++ src/components/Form/Form-UIs/FormLayout.tsx
@@ -239,6 +239,8 @@ const FormLayout = ({
                   setFillingFormState={setFillingFormState}
                   publishedFormState={publishedFormState}
                   fillingFormState={fillingFormState}
+                  setBackupForSwitchFormState={setBackupForSwitchFormState}
+                  backupForSwitchFormState={backupForSwitchFormState}
                 />
               )
             }
