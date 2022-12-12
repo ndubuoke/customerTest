802a3ae25379c3750b7e9677621a7b54644d9b1c
Adeshina's <badmusadeshinna@gmail.com>
2022-12-12 09:15:13 +0100

    all fields for executive/directors before functionality

--- src/components/IndependentForms/Sharables/IndependentFormDate.tsx
+++ src/components/IndependentForms/Sharables/IndependentFormDate.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 type Props = {}
 
 const FormDate = (props: Props) => {
-  return <div>FormDate</div>
+  return (
+    <div className='border-b'>
+      <input type='date' className=' cursor-pointer bg-transparent border-none outline-none py-1 ' disabled />
+    </div>
+  )
 }
 
 export default FormDate
