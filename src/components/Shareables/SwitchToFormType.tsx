079e3b1c82f32f0ae6e8c289fd81a235823381b7
Bonaventure Chukwudi <Bonarhyme@gmail.com>
2022-12-21 13:26:42 +0100

    added switch form modal

--- src/components/Shareables/SwitchToFormType.tsx
+++ src/components/Shareables/SwitchToFormType.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CustomerType, FormModeType } from 'Screens/CustomerCreation'
 import { formTypeSwitch as formTypeSwitchImage } from 'Assets/svgs'
 import { getFormAction } from 'Redux/actions/FormManagement.actions'
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux'
 import { formStruture } from 'Components/Form/formStructure'
 import { STORAGE_NAMES } from 'Utilities/browserStorages'
 import { FormStructureType } from 'Components/types/FormStructure.types'
+import SwitchFormModal from './SwitchFormModal'
 
 type Props = {
   mode: FormModeType
@@ -29,16 +30,19 @@ const SwitchToFormType = ({
 }: Props) => {
   const dispatch = useDispatch()
 
-  const handleSetFormType = (formMode: FormModeType) => {
-    // const theMode = formMode === 'accelerated' ? 'legacy' : 'accelerated'
-    if (!window.confirm(`Switch to ${formMode} form? The information captured so far will be transferred to the ${formMode} form`)) {
-      return
-    }
+  const [openModal, setOpenModal] = useState<boolean>(false)
 
+  const handleOpenModal = () => {
+    setOpenModal((prev) => !prev)
+  }
+
+  const handleSetFormType = () => {
+    const formMode = mode === 'accelerated' ? 'legacy' : 'accelerated'
     setFillingFormState(formStruture)
     sessionStorage.removeItem(STORAGE_NAMES.FILLING_FORM_IN_STORAGE)
     sessionStorage.removeItem(STORAGE_NAMES.PUBLISHED_FORM_IN_STORAGE)
     setPublishedFormState(null)
+    handleOpenModal()
 
     if (formMode === 'legacy') {
       onSetFormMode(formMode)
@@ -59,19 +63,32 @@ const SwitchToFormType = ({
   }
 
   return (
-    <div
-      onClick={() => {
-        mode === 'accelerated' ? handleSetFormType('legacy') : handleSetFormType('accelerated')
-      }}
-      className='absolute right-2 top-2 max-w-[179px]  rounded-lg border border-[#EBE9E9] flex items-center gap-2 p-[18px] pr-2 cursor-pointer bg-white z-50'
-    >
-      <div>
-        <img src={formTypeSwitchImage} />
+    <>
+      <div
+        onClick={(e) => {
+          e.stopPropagation()
+          handleOpenModal()
+          // mode === 'accelerated' ? handleSetFormType('legacy') : handleSetFormType('accelerated')
+        }}
+        className='absolute right-2 top-2 max-w-[179px]  rounded-lg border border-[#EBE9E9] flex items-center gap-2 p-[18px] pr-2 cursor-pointer bg-white z-50'
+      >
+        <div>
+          <img src={formTypeSwitchImage} />
+        </div>
+        <span className='text-[14px] min-w-[110px] font-roboto text-center font-normal'>
+          Switch to {mode === 'accelerated' ? 'legacy' : 'accelerated'} form
+        </span>
       </div>
-      <span className='text-[14px] min-w-[110px] font-roboto text-center font-normal'>
-        Switch to {mode === 'accelerated' ? 'legacy' : 'accelerated'} form
-      </span>
-    </div>
+      {openModal ? (
+        <SwitchFormModal
+          message={` Switch to ${
+            mode === 'accelerated' ? 'legacy' : 'accelerated'
+          } form? The information captured so far will be transferred to the ${mode === 'accelerated' ? 'legacy' : 'accelerated'} form`}
+          closeModalFunction={handleOpenModal}
+          switchFunction={handleSetFormType}
+        />
+      ) : null}
+    </>
   )
 }
 
