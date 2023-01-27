d7b6a109fa0d98b826a7f2bce6a1bd4fa15173d7
owoh princewill <princewillowoh@gmail.com>
2023-01-27 16:06:35 +0100

    changed bg to white

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -391,7 +391,7 @@ const Main = (props: Props) => {
       ) : null}
 
       <div className='  flex flex-col  bg-white'>
-        <div className=' flex w-[62.5rem] mt-10 pl-6 items-center '>
+        <div className=' flex w-[62.5rem] mt-10 pl-6 items-center'>
           <h1 className='text-[#636363] text-[2.375rem] font-bold'>Customer Management</h1>
 
           {userRole === 'maker' && (
