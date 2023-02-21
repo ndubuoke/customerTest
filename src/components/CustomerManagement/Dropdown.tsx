e1163b9f6a4f61c29f2c9ab51fccc9121bd687a9
owoh princewill <princewillowoh@gmail.com>
2023-02-21 16:04:04 +0100

    created Dropdown component in the product assignment page

--- src/components/CustomerManagement/Dropdown.tsx
+++ src/components/CustomerManagement/Dropdown.tsx
@@ -1,29 +1,41 @@
 import { caret } from 'Assets/svgs'
-import { FormControlType } from 'Components/types/FormControl.types'
 import { memo } from 'react'
 
-type DropdownType = {}
+type DropdownType = {
+  showLists: boolean
+  data: []
+  selectedItemHandler: (e) => void
+}
 
-const Dropdown = memo(({}: DropdownType) => {
+const Dropdown = memo(({ showLists, data, selectedItemHandler }: DropdownType) => {
   return (
-    <div className='w-full p-1 '>
-      <div className='w-full '>
-        <button className='flex items-center justify-between w-full gap-6 py-1 leading-6 border-b border-b-text-secondary' title={'fuck'}>
-          <div
-            className={`text-text-disabled capitalize`}
-            style={{
-              whiteSpace: 'nowrap',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-            }}
-          >
-            {'enter a placeholder'}
-          </div>
-          <span>
-            <img src={caret} width={15} height={10} />
-          </span>
-        </button>
-      </div>
+    <div className='  '>
+      <button className='flex items-center justify-between w-[300px]  gap-6 py-1 leading-6 border-b border-b-text-secondary' title={'fuck'}>
+        <div
+          className={`text-text-disabled capitalize`}
+          style={{
+            whiteSpace: 'nowrap',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
+        >
+          {'enter a placeholder'}
+        </div>
+        <span>
+          <img src={caret} width={15} height={10} />
+        </span>
+      </button>
+      {showLists && (
+        <div className='absolute w-full top-0   bg-background-paper  flex flex-col z-20 border rounded-md'>
+          {data?.map((list, index) => {
+            return (
+              <div key={index} className='hover:bg-[#FFD4D2] cursor-pointer px-3 py-2' onClick={selectedItemHandler.bind(null, list)}>
+                {list}
+              </div>
+            )
+          })}
+        </div>
+      )}
     </div>
   )
 })
--- src/components/CustomerManagement/ProductAssignment.tsx
+++ src/components/CustomerManagement/ProductAssignment.tsx
@@ -3,9 +3,20 @@ import convertToUppercase from 'Utilities/convertToUppercase'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import SearchAndSelect from './SearchAndSelect'
+import Dropdown from './Dropdown'
+import { useState, useEffect } from 'react'
+
 
 const ProductAssignment = () => {
+  const [showLists, setShowLists] = useState(false)
+  
   let { customerType } = useParams()
+  const selectedItemHandler = (item) => {
+    console.log(item)
+  }
+  useEffect(()=>{
+
+  },[])
 
   return (
     <div className=' h-screen bg-[#E5E5E5] w-full '>
@@ -21,9 +32,9 @@ const ProductAssignment = () => {
       </div>
       <div className='mx-[2%] my-[2%] flex gap-8'>
         <div className=' w-[100%] bg-white py-[2%] overflow-auto   rounded-md '>
-          
-          <div className='border w-full flex gap-4'>
-            <SearchAndSelect fieldlabel=''/>
+          <div className=' w-full flex gap-12 justify-center items-center'>
+            <h1 className='font-normal  text-[18px]'>Product Type</h1>
+            <Dropdown showLists={showLists} data={[]} selectedItemHandler={selectedItemHandler} />
           </div>
         </div>
       </div>
