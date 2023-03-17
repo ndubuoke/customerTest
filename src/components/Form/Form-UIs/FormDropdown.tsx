7d784ab7f69b623061bb00e911e245e9f4607b9b
Favourene <igbinosaiwinosa01@gmail.com>
2023-03-18 00:16:59 +0100

    added a fall back image for when user selects a pdf

--- src/components/Form/Form-UIs/FormDropdown.tsx
+++ src/components/Form/Form-UIs/FormDropdown.tsx
@@ -671,7 +671,7 @@ const FormDropdown = ({
                   selectedDropdownItem
                 )
               ) : (
-                <span className={`text-text-disabled`}>Selectz</span>
+                <span className={`text-text-disabled`}>Select</span>
               )}
             </div>
           ) : null}
