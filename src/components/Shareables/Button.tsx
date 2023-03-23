81a872315d30f05feaa702c559bc87c3f1180d2e
Adeshina's <badmusadeshinna@gmail.com>
2023-03-23 12:28:27 +0100

    fixes

--- src/components/Shareables/Button.tsx
+++ src/components/Shareables/Button.tsx
@@ -11,8 +11,8 @@ const Button = ({ text = 'Proceed', disabled, onClick }: Props) => {
 
   return (
     <button
-      className={`font-bold leading-5 text-white   cursor-pointer h-full w-fit px-10 py-[0.4rem]   rounded-lg ${
-        disabled ? 'cursor-not-allowed ' : ` ${text === 'Previous' ? 'previous-btn' : 'bg-button-hover-background'} `
+      className={`font-bold leading-5    cursor-pointer h-full w-fit px-10 py-[0.4rem]   rounded-lg ${
+        disabled ? 'cursor-not-allowed ' : ` ${text === 'Previous' ? 'previous-btn' : 'bg-button-hover-background text-white'} `
       }`}
       style={{
         fontWeight: '500',
